16a5a095c42afe763f61fc7cb86d78cf151a4c4c
BBlackwave <patrick.smith12@yahoo.com>
2024-12-02 15:14:35 -0600

    deleted data

--- app.py
+++ app.py
@@ -21,14 +21,7 @@ JOBS = [
   'location': 'Remote',
   'salary': 'Rs. 12,00,000'
   },
-  {
-  'id': 4,
-  'title': 'Backend Engineer',
-  'location': 'San Francisco, USA',
-  'salary': '$150,000'
-    
-  
-  },
+
 
 ]
 
